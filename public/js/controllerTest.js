d47a3baec72e1e2acb683c4ac31762d118fb22a4
asteph <alissa.stephens1@gmail.com>
2015-09-08 11:41:11 -0500

    add item button appears if item name and cost have values; new item is pushed to the item array and new item object is reset; TODO: allow quantity to be changed

--- public/js/controllerTest.js
+++ public/js/controllerTest.js
@@ -6,25 +6,34 @@
     app.controller("CartController", function() {
         this.items = [ 
         { 
-          name: 'apple', 
-          price: 0.50,
-          quantity: 1,
+            name: 'apple', 
+            price: 0.50,
+            quantity: 1,
         }, 
         { 
-          name: 'orange', 
-          price: 0.75,
-          quantity: 3,
+            name: 'orange', 
+            price: 0.75,
+            quantity: 3,
         }, 
         { 
-          name: 'banana', 
-          price: 1.00,
-          quantity: 1,
+            name: 'banana', 
+            price: 1.00,
+            quantity: 1,
         }, 
         { 
-          name: 'pineapple', 
-          price: 2.50,
-          quantity: 2,
+            name: 'pineapple', 
+            price: 2.50,
+            quantity: 2,
         }, 
         ];
+        this.newItem = {
+        	quantity: 1
+        };
+        this.addItem = function(){
+        	this.items.push(this.newItem);
+        	this.newItem = {
+        		quantity:1
+        	};
+        }
     });
 })();
